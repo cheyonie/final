711a27520224eda590a8bf117266689cf57ed0c6
cheyonie <132964287+cheyonie@users.noreply.github.com>
2023-07-28 23:22:18 -0500

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,12 @@
+function getdetails()
+        {
+var a = document.forms["contact"]["firstname"].value;
+var b = document.forms["contact"]["lastname"].value;
+var c= document.forms["contact"]["number"].value;
+
+alert("Thank you for your request "+a+" "+b+"! We will contact you asap!");
+
+        }
+
+
+  
